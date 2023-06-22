4820c58ba5d2d4c107348fe3063eb39f43d61c67
Hyunjae Lee <linked0@gmail.com>
2023-06-22 18:35:12 +0900

    Use copilot

---
+++ site/parse_expenses.py
@@ -0,0 +1,17 @@
+import datetime
+# use factorial function from math module
+from math import factorial
+
+
+def parse_expenses(expenses_string):
+  """Parse the list of expenses and return the list of triples (date, value, currency).
+  Ignore lines starting with #.
+  Parse the date using datetime.
+  """
+
+def main():
+  # use factiorial function
+  print(factorial(5))
+
+if __name__ == "__main__":
+  main()
